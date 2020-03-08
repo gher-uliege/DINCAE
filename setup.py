c44dd139f8092f19a5ffaf8975db26859e9c167b
Alexander Barth <barth.alexander@gmail.com>
2020-03-08 20:31:54 +0100

    update licence in setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setuptools.setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL-3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
